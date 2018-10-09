fbe349a8ec69c3ff9fd6a2e469dade39be1c9f3a
igor <ig.plotnikov@gmail.com>
2018-10-09 09:24:27 +0300

    Initial commit

---
+++ lib/thing.rb
@@ -0,0 +1,20 @@
+class Thing
+  def initialize(file)
+    file = File.readlines(file, encoding: 'UTF-8', chomp: true)
+    @name = file[0]
+    @type = file[1]
+    @wheather_range = file[2]
+  end
+
+  def name
+    @name
+  end
+
+  def type
+    @type
+  end
+  def wheather_range
+    @wheather_range
+  end
+
+end
